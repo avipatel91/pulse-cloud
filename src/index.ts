b15211c7b100ed87abd4bb9c22f23947cdb73a30
Avi Patel <avipatel91@gmail.com>
2019-09-09 23:37:38 -0700

    Revert "Add database"

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import apiRouter from './routes'
-import { handleAuthError } from './middlewares/errorHandlers'
-import { authenticate } from './middlewares/auth'
-import admin from 'firebase-admin'
-
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: 'https://beatboard-app.firebaseio.com',
-})
+// import loginRoute from './routes/login'
 
 const app = express()
 app.use(bodyParser.json())
-
-if (process.env.NODE_ENV !== 'development') {
-  app.use(authenticate)
-}
-app.use(apiRouter)
-app.use(handleAuthError)
+// app.use('/', loginRoute)
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
--- src/utils/wrapAsync.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from 'express'
-
-export default function wrapAsync(fn: RequestHandler): RequestHandler {
-  const wrappedFn: RequestHandler = (req, res, next) => {
-    fn(req, res, next).catch(next)
-  }
-  return wrappedFn
-}
