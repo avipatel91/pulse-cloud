c8f09a893d0e4e3c0686e26c8ffd9f15954c9747
Avi Patel <avipatel91@gmail.com>
2019-10-17 23:26:31 -0700

    Add validation for POST of session
    I enure that member id in params = id from token.
    I ensure user is type member.
    I ensure sessionCode is present.
    I ensure sessionCode exists.

--- src/routes/v1/member.ts
+++ src/routes/v1/member.ts
@@ -1,17 +1,34 @@
 import express, { RequestHandler } from 'express'
+import { getRepository } from 'typeorm'
 import wrapAsync from '../../utils/wrapAsync'
-import { ForbiddenError } from '../../common/Errors'
+import { ForbiddenError, BadRequestError } from '../../common/Errors'
+import { Session } from '../../entity/Session'
 
 const router = express.Router()
 
 const ensureIsMember: RequestHandler = async (req, res, next) => {
-  if (req.user!.type !== 'member') {
+  if (req.user.type !== 'member') {
     throw new ForbiddenError('Must be a member')
+  }
+  if (req.user.id !== req.params.memberId) {
+    throw new ForbiddenError('Unauthorized')
   }
   next()
 }
 
-const ensureClassCode: RequestHandler = async (req, res, next) => {
+const ensureSessionCode: RequestHandler = async (req, res, next) => {
+  const { sessionCode } = req.body
+  if (!sessionCode) {
+    throw new BadRequestError('Session code must be present')
+  }
+  const session = await getRepository(Session)
+    .createQueryBuilder()
+    .select('session.id')
+    .from(Session, 'session')
+    .where('session."sessionCode" = :sessionCode', { sessionCode })
+    .getOne()
+  if (!session) throw new BadRequestError('Invalid session code')
+  req.session = { id: session.id }
   next()
 }
 
@@ -21,7 +38,7 @@ const addSession: RequestHandler = async (req, res) => {
 
 router.post('/:memberId/session', [
   wrapAsync(ensureIsMember),
-  wrapAsync(ensureClassCode),
+  wrapAsync(ensureSessionCode),
   wrapAsync(addSession),
 ])
 
