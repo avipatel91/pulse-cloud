65432a339ea3a446fa60d2f58163b2147db71df7
Avi Patel <avipatel91@gmail.com>
2019-10-20 02:24:34 -0700

    Add GET, POST, and DELETE to member session.
    GET: gets all session for a member
    POST: register member to a session
    DELETE: remove member from a session

--- src/routes/v1/member.ts
+++ src/routes/v1/member.ts
@@ -1,8 +1,9 @@
 import express, { RequestHandler } from 'express'
-import { getRepository } from 'typeorm'
+import { getRepository, createQueryBuilder } from 'typeorm'
 import wrapAsync from '../../utils/wrapAsync'
 import { ForbiddenError, BadRequestError } from '../../common/Errors'
 import { Session } from '../../entity/Session'
+import { Member } from '../../entity/Member'
 
 const router = express.Router()
 
@@ -33,13 +34,53 @@ const ensureSessionCode: RequestHandler = async (req, res, next) => {
 }
 
 const addSession: RequestHandler = async (req, res) => {
+  await createQueryBuilder()
+    .relation(Session, 'members')
+    .of(req.session!.id)
+    .add(req.user.id)
+    .catch(e => {
+      let errorMessage = ''
+      if (/already exists/.test(e.detail)) {
+        errorMessage = 'Member is already registered to session.'
+      }
+      throw new Error(errorMessage)
+    })
   res.sendStatus(200)
 }
 
-router.post('/:memberId/session', [
+const getSessions: RequestHandler = async (req, res) => {
+  const sessions = await createQueryBuilder()
+    .select('session.id')
+    .addSelect('session.name')
+    .from(Session, 'session')
+    .innerJoin('member_sessions_session', 'm_s', 'm_s.sessionId = session.id')
+    .where('m_s.memberId = :memberId', { memberId: req.user.id })
+    .getRawMany()
+  res.send(sessions)
+}
+
+const removeSession: RequestHandler = async (req, res) => {
+  await createQueryBuilder()
+    .relation(Session, 'members')
+    .of(req.params.sessionId)
+    .remove(req.user.id)
+  res.sendStatus(200)
+}
+
+router.get('/:memberId/sessions', [
+  wrapAsync(ensureIsMember),
+  wrapAsync(getSessions),
+])
+
+router.post('/:memberId/sessions', [
   wrapAsync(ensureIsMember),
   wrapAsync(ensureSessionCode),
   wrapAsync(addSession),
+])
+
+router.delete('/:memberId/sessions/:sessionId', [
+  wrapAsync(ensureIsMember),
+  wrapAsync(removeSession),
 ])
 
 export default router
